29ac82052e4e12bf0a9c510701ac44f8e06372cb
d05660 <d05660@163.com>
2019-06-24 13:13:28 +0800

    add 404 403 500.
    Signed-off-by: d05660 <d05660@163.com>

--- springbootDemo2/src/main/java/org/cloud/system/controller/IndexController.java
+++ springbootDemo2/src/main/java/org/cloud/system/controller/IndexController.java
@@ -6,6 +6,7 @@ import org.cloud.system.service.IMenuService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.context.WebApplicationContext;
 
 @Controller
 public class IndexController {
@@ -18,5 +19,23 @@ public class IndexController {
         model.addAttribute("menus", service.getTreeData(5));
         return "admin/index";
     }
-    
+
+    @Resource
+    private WebApplicationContext applicationContext;
+
+    @GetMapping("/403")
+    public String forbidden() {
+        return "error/403";
+    }
+
+    @GetMapping("/404")
+    public String unauthorizedPage() {
+        return "error/404";
+    }
+
+    @GetMapping("/500")
+    public String error() {
+        return "error/500";
+    }
+
 }
